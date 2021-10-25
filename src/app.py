4d7537f78450e9f62d62ab1c05da3a6579f8e2e5
walterjesus88 <wjesus88@gmail.com>
2021-10-25 16:18:37 -0500

    *

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path=str(os.environ.get("CHROMEDRIVER_PATH")),chrome_options=options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
 
 
 driver.get("https://youtube.com")
