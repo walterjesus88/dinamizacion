cc4c50f6d78117ec91e3a049f737437cf2c2f80f
walterjesus88 <wjesus88@gmail.com>
2021-10-25 16:23:18 -0500

    *

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
-import os
 from selenium import webdriver
+import os
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 options.add_argument('--headless')
-options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
 
 
