b8013ef397bebfaeb387d1f24bfc92551a1aab30
walterjesus88 <wjesus88@gmail.com>
2021-10-29 11:02:08 -0500

    ff

--- src/app.py
+++ src/app.py
@@ -42,7 +42,7 @@ def index():
     options = webdriver.ChromeOptions()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
