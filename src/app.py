075fc272ad8928c18193b7dbe5c3b2fbd301d164
walterjesus88 <wjesus88@gmail.com>
2021-10-29 08:43:16 -0500

    test

--- src/app.py
+++ src/app.py
@@ -40,13 +40,13 @@ def login(browser):
 @app.route('/')
 def index(): 
     options = webdriver.ChromeOptions()
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    #options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
-    #driver = webdriver.Chrome(executable_path="./chromedriver")
+    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
+    driver = webdriver.Chrome(executable_path="./chromedriver")
 
 
 
@@ -58,8 +58,10 @@ def index():
     time.sleep(5)
     driver.find_element_by_xpath('//*[@id="youbora__home__app__analytics"]').click()
 
-    element_to_hover_over = driver.find_element_by_id("sidebar").click()
+    #element_to_hover_over = driver.find_element_by_id("sidebar").click()
+
     #
+    driver.find_element_by_xpath('//*[@id="sidebar"]').click()
     driver.find_element_by_xpath('//*[@id="sidebar-pinner"]').click()
     #hover = ActionChains(driver).move_to_element(element_to_hover_over)
     #hover.perform()
