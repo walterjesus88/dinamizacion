9583db9ec3ff47d778a0901cec3d26cb07ea37d0
walterjesus88 <wjesus88@gmail.com>
2021-10-29 08:09:52 -0500

    test

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ app = Flask(__name__,template_folder='../template')
 def inc(x):
     return x + 1
 
-def _login(browser):
+def login(browser):
 
     #browser.get("https://suite.npaw.com/login")
     #browser.maximize_window()
@@ -27,25 +27,26 @@ def _login(browser):
  
 
     #print('browser')
-    #browser.find_element_by_xpath('//*[@id="youbora__home__app__analytics"]').click()
-    #browser.find_element_by_xpath('//*[@id="sidebar"]/div[2]/div[1]/div/div/input').send_keys("PeruOps")
-    #time.sleep(2)
 
-    browser.execute_script("arguments[0].click();", WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="youbora__container"]/main/div[1]/button'))))
-    browser.execute_script("arguments[0].click();", WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/button[2]'))))
+  
+
+
+
+    #browser.execute_script("arguments[0].click();", WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="youbora__container"]/main/div[1]/button'))))
+    #browser.execute_script("arguments[0].click();", WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[3]/div/div[3]/button[2]'))))
 
     return browser
 
 @app.route('/')
 def index(): 
     options = webdriver.ChromeOptions()
-    #options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
-    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
-    driver = webdriver.Chrome(executable_path="./chromedriver")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
+    #driver = webdriver.Chrome(executable_path="./chromedriver")
 
 
 
@@ -55,9 +56,29 @@ def index():
     driver.find_element_by_xpath('//*[@id="youbora__container"]/div[1]/form/div[2]/div/input').send_keys("P3ru0ps")
     driver.find_element_by_xpath('//*[@id="youbora__login_submit"]').click()
     time.sleep(5)
-    driver.get("https://suite.npaw.com/analytics/MainKPIsPeru/Phantasia-DINA")
+    driver.find_element_by_xpath('//*[@id="youbora__home__app__analytics"]').click()
+
+    element_to_hover_over = driver.find_element_by_id("sidebar")
+
+    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+    hover.perform()
+
+    #driver.find_element_by_xpath('//*[@id="sidebar"]/div[2]/div[1]/div/div/input').()
+
+    driver.find_element_by_xpath('//*[@id="sidebar"]/div[2]/div[1]/div/div/input').send_keys("Phantasia")
+    driver.find_element_by_xpath('//*[@id="sidebar"]/div[1]/div/div[1]/div/div/div[3]/div').click()
+    driver.find_element_by_xpath('//*[@id="sidebar"]/div[1]/div/div[1]/div/div/div[7]/div/a/div').click()
+    element_to_hover_over = driver.find_element_by_id("youbora__container")
+
+    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+    hover.perform()
+
+
+    #driver.get("https://suite.npaw.com/analytics/MainKPIsPeru/Phantasia-DINA")
+    driver.find_element_by_xpath('//*[@id="youbora__container"]/main/div[1]/button').click()
+    driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div[3]/button[2]').click()
     #driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="youbora__login_submit"]'))))
-    browser = _login(driver)
+    #browser = login(dri)
 
     return render_template(
         'form.html')
